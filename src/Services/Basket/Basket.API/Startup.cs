84bcb6c0601ecb170c9ce680dea858c278fa4343
bs672027 <bharatsingh.mca@gmail.com>
2021-06-22 15:36:07 +0100

    Added Basket Repositories

--- src/Services/Basket/Basket.API/Startup.cs
+++ src/Services/Basket/Basket.API/Startup.cs
@@ -1,10 +1,22 @@
+//using Basket.API.GrpcServices;
+using Basket.API.Repositories;
+//using Discount.Grpc.Protos;
+//using HealthChecks.UI.Client;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Basket.API
 {
@@ -25,6 +37,10 @@ namespace Basket.API
             {
                 options.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString");
             });
+
+            // General Configuration
+            services.AddScoped<IBasketRepository, BasketRepository>();
+           // services.AddAutoMapper(typeof(Startup));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
