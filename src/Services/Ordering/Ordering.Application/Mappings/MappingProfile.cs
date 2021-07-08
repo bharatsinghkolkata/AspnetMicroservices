e69273d441ba09d5e0241336f29fc1aff18c9ad4
bs672027 <bharatsingh.mca@gmail.com>
2021-07-08 11:16:05 +0100

    Added Expections, Feature folder in Ordering.Application Project

--- src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
+++ src/Services/Ordering/Ordering.Application/Mappings/MappingProfile.cs
@@ -1,4 +1,7 @@
 ﻿using AutoMapper;
+using Ordering.Application.Features.Orders.Commands.CheckoutOrder;
+using Ordering.Application.Features.Orders.Commands.UpdateOrder;
+using Ordering.Application.Features.Orders.Queries.GetOrdersList;
 using Ordering.Domain.Entities;
 
 namespace Ordering.Application.Mappings
@@ -7,9 +10,9 @@ namespace Ordering.Application.Mappings
     {
         public MappingProfile()
         {
-            //CreateMap<Order, OrdersVm>().ReverseMap();
-            //CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
-            //CreateMap<Order, UpdateOrderCommand>().ReverseMap();
+            CreateMap<Order, OrdersVm>().ReverseMap();
+            CreateMap<Order, CheckoutOrderCommand>().ReverseMap();
+            CreateMap<Order, UpdateOrderCommand>().ReverseMap();
         }
     }
 }
