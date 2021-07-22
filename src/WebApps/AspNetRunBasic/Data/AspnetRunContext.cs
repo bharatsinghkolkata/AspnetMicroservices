60f0961fb3681026cb7509247a42c2918e395a9d
bs672027 <bharatsingh.mca@gmail.com>
2021-07-22 12:26:39 +0100

    Added WebApps Project

---
+++ src/WebApps/AspNetRunBasic/Data/AspnetRunContext.cs
@@ -0,0 +1,16 @@
+﻿using AspNetRunBasic.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetRunBasic.Data
+{
+    public class AspnetRunContext : DbContext
+    {
+        public AspnetRunContext(DbContextOptions<AspnetRunContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ src/WebApps/AspNetRunBasic/Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNetRunBasic.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public IndexModel()
+        {
+        }
+
+        public async Task<IActionResult> OnGet()
+        {
+            return Page();
+        }
+    }
+}
